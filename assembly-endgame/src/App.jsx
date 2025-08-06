efd9c57d746e9b6957c361a7f413fd00de55226b
Andrew Shafer <andrewshafer@Andrews-MBP.localdomain>
2025-08-05 20:59:56 -0400

    Header changes

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -19,10 +19,18 @@ import React from "react"
  * 
  */
 
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
+ */
+
 export default function Hangman() {
     return (
         <main>
-            Game goes here
+            <h2>Assembly: Endgame</h2>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </main>
     )
 }
